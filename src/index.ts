42368f062357914a6770fc48bc4ee9c7fcc0861e
Matthias Hampel <matthiashampel@icloud.com>
2026-05-15 22:53:25 +0200

    do not export redis

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export * from "./heartbeat/heartbeat.module";
 export * from "./heartbeat/heartbeat.service";
 export * from "./raft.module";
 export * from "./raft-options";
-export * from "./redis/redis.module";
-export * from "./redis/redis.service";
